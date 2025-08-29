94b8e6d8349e7d3b49ed2fa5cd09eab8b4e1a3e1
FahimZaryab <tamu8167@gmail.com>
2025-08-29 20:31:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ MONGO_DB_URI = getenv(
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 10000000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", -1002734621918))
+LOGGER_ID = int(getenv("LOGGER_ID", -1003084307535))
 
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6037958673))
